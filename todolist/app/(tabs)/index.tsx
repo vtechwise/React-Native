caed529b3dfff93f0e4958577971d391e9de1c58
victor oguntayo <oguntayovictor65@gmail.com>
2026-03-28 19:39:35 +0100

    add Index component for todolist screen

---
+++ todolist/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "yellow",
+  },
+});
