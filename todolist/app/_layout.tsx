7623575d0a9d7df6aedf841bb3bbe0e2b0a0d133
victor oguntayo <oguntayovictor65@gmail.com>
2026-03-28 19:39:20 +0100

    add RootLayout component for navigation structure

---
+++ todolist/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { HeaderShownContext } from "@react-navigation/elements";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{ title: "Home", headerShown: false }}
+      />
+    </Stack>
+  );
+}
