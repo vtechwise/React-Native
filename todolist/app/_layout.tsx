7d3d0524d4165b6deb05a46b0938672d286ac873
victor oguntayo <oguntayovictor65@gmail.com>
2026-03-29 18:56:22 +0100

    feat: integrate Convex provider, implement theme persistence with AsyncStorage, and add dark mode toggle UI

--- todolist/app/_layout.tsx
+++ todolist/app/_layout.tsx
@@ -1,16 +1,23 @@
 import { HeaderShownContext } from "@react-navigation/elements";
 import { Stack } from "expo-router";
 import { ThemeProvider } from "./hooks/useTheme";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{ title: "Home", headerShown: false }}
-        />
-      </Stack>
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack>
+          <Stack.Screen
+            name="(tabs)"
+            options={{ title: "Home", headerShown: false }}
+          />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
   );
 }
