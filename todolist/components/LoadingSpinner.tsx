d6aed22346dfc544d49cbe943041ade13d1bf4c6
victor oguntayo <oguntayovictor65@gmail.com>
2026-03-30 19:36:48 +0100

    feat: add LoadingSpinner and TodoInput components for task management interface

---
+++ todolist/components/LoadingSpinner.tsx
@@ -0,0 +1,24 @@
+import useTheme from "@/app/hooks/useTheme";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import { LinearGradient } from "expo-linear-gradient";
+import { ActivityIndicator, Text, View } from "react-native";
+
+const LoadingSpinner = () => {
+  const { colors } = useTheme();
+
+  const homeStyles = createHomeStyles(colors);
+
+  return (
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <View style={homeStyles.loadingContainer}>
+        <ActivityIndicator size="large" color={colors.primary} />
+        <Text style={homeStyles.loadingText}>Loading your todos...</Text>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default LoadingSpinner;
