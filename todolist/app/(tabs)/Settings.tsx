6cf78b5cb2934f194d9d8f120b4202c2c34f7dc0
victor oguntayo <oguntayovictor65@gmail.com>
2026-03-31 14:05:20 +0100

    feat: add ProgressStat and Preferences components to the Settings screen

--- todolist/app/(tabs)/Settings.tsx
+++ todolist/app/(tabs)/Settings.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import useTheme from "../hooks/useTheme";
+import { LinearGradient } from "expo-linear-gradient";
+// import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import ProgressStat from "@/components/ProgressStat";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Preferences from "@/components/Preferences";
 
 const Settings = () => {
+  const { colors, isDarkMode, toggleDarkMode } = useTheme();
+
+  const settingsStyle = createSettingsStyles(colors);
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingsStyle.container}
+    >
+      <SafeAreaView>
+        {/* Header */}
+        <View style={settingsStyle.header}>
+          <View style={settingsStyle.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingsStyle.iconContainer}
+            >
+              <Ionicons name="settings" size={28} color={"#ffff"} />
+            </LinearGradient>
+            <Text style={settingsStyle.title}>Settings</Text>
+          </View>
+        </View>
+        {/* scroll view */}
+        <ScrollView
+          // style={settingsStyle.scrollView}
+          contentContainerStyle={settingsStyle.content}
+          showsVerticalScrollIndicator={false}
+        >
+          <ProgressStat />
+          <Preferences />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
