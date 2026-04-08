c4ec600cb46e5432a2fc294875bd7ee016776f5f
victor oguntayo <oguntayovictor65@gmail.com>
2026-04-08 04:13:41 +0100

    feat: implement Clerk authentication flow with sign-in and sign-up routes

--- Wallet/mobile/app/_layout.tsx
+++ Wallet/mobile/app/_layout.tsx
@@ -1,10 +1,23 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import SafeScreen from "../components/SafeScreen";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error("missing clekr publishablekey");
+}
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeScreen>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={publishableKey as string}
+    >
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
   );
 }
